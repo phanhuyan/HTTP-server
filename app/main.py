86296968c914d63880cead7e90f2093cb8cbf4de
phanhuyan <justawayww@gmail.com>
2023-10-21 21:02:28 -0500

    task4

--- app/main.py
+++ app/main.py
@@ -1,7 +1,20 @@
 # Uncomment this to pass the first stage
 import socket
 
-
+def parse_http_request(data):
+    request_lines = data.split('\r\n')
+    start_line = request_lines[0]
+    
+    method, path, protocol = start_line.split(' ')
+    
+    headers = {}
+    
+    for line in request_lines[1:]:
+        if line.strip():
+            key, value = line.split(': ', 1)
+            headers[key] = value
+    
+    return method, path, protocol, headers
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
@@ -17,15 +30,18 @@ def main():
                 continue  # If no data is received, continue to the next iteration
 
             # Split the request into lines and get the first line (start line)
-            request_lines = data.split('\r\n')
-            start_line = request_lines[0]
 
             # Extract the path from the start line
-            _, path, _ = start_line.split(' ')
-
+            method, path, protocol, headers = parse_http_request(data)
             # Check if the path is '/'
             if path == "/":
                 response = "HTTP/1.1 200 OK\r\n\r\n"
+            elif path == '/user-agent':
+                user_agent = headers.get('User-Agent', 'Unknown User-Agent')
+                response = f"HTTP/1.1 200 OK\r\n"
+                response += "Content-Type: text/plain\r\n"
+                response += f"Content-Length: {len(user_agent)}\r\n\r\n"
+                response += user_agent
             elif path.startswith('/echo/'):
                 random_string = path[6:]  # Extract the random string
                 response = "HTTP/1.1 200 OK\r\n"
