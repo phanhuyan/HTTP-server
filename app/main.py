bb029e726f0341525de6ceca2b42ff175702c329
phanhuyan <justawayww@gmail.com>
2023-10-21 20:39:06 -0500

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 # Uncomment this to pass the first stage
 import socket
-MAX_CONNECTIONS = 10
+
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -9,13 +9,9 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.listen(MAX_CONNECTIONS)
-    while True:
-        client_socket, client_address = server_socket.accept()
-        print(f"Accepted connection from {client_address}")
-        response = "HTTP/1.1 200 OK\r\n\r\n"
-        client_socket.send(response.encode())
-        client_socket.close()
+    conn, addr = server_socket.accept()  # wait for client
+    with conn:
+        conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
 
 if __name__ == "__main__":
     main()
